e936efd02a064c0ce10c1e1da39ddbab085146f8
Gabriel Ramires <gabrielramiresdeoliveira1@gmail.com>
2022-03-27 11:54:48 -0300

    Cool Commit

--- bin/CircleCI/jobs/StartUp/StartUp.sh
+++ bin/CircleCI/jobs/StartUp/StartUp.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
-echo hello world!!
-jekyll server
-
-dir
-input="bin/CircleCI/jobs/StartUp/configurations.conf"
-while IFS= read -r line
-do
-  echo "$line"
-done < "$input"
+echo well...
+echo Start up again :)
+echo none checkup its here because i don't know how use Jekyll with CircleCI xd
+echo <3
+echo powered by Jekyll & Github
